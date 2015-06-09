22ea4d6b783e794feca49098aced7180d7e09c39
Víctor Manuel Fernández Castro <vmfdez90@gmail.com>
2015-06-09 21:07:53 +0200

    Simulación del control de reproducción con Javascript

--- manager/player.php
+++ manager/player.php
@@ -24,7 +24,7 @@ html_navigation('player');
 echo <<< EOT
 <section>
     <div id="player-control">
-        <h3 id="part-name">Pieza con un nombre extremadamente largo y sin apellidos, Fulgencio Marjangen Op. 34 nº 75</h3>
+        <h3 id="part-name">-</h3>
         <input type="button" class="player-button" id="bt-previuos" title="{$tr['previuos']}" onclick="previuos()">
         <input type="button" class="player-button" id="bt-play" title="{$tr['play']}" onclick="play()">
         <input type="button" class="player-button" id="bt-pause" title="{$tr['pause']}" onclick="pause()">
@@ -33,12 +33,12 @@ echo <<< EOT
     </div>
     <div id="player-playlist">
         <h2>{$tr['current_playlist']}</h2>
-        <ul>
-            <li><a href="">Tengo un tractor amarillo, pero soy daltónico</a></li>
-            <li class="selected"><a href="">Pieza con un nombre extremadamente largo y sin apellidos, Fulgencio Marjangen Op. 34 nº 75</a></li>
-            <li><a href="">Toccata e fugue</a></li>
-            <li><a href="">Marcha fúnebre para homilía</a></li>
-            <li><a href="">Hallelujah (la de Alexandra Burke)</a></li>
+        <ul id="current-playlist">
+            <li id="part-1" onclick="select(1)">Tengo un tractor amarillo, pero soy daltónico</li>
+            <li id="part-2" onclick="select(2)">Pieza con un nombre extremadamente largo y sin apellidos, Fulgencio Marjangen Op. 34 nº 75</li>
+            <li id="part-3" onclick="select(3)">Toccata e fugue</li>
+            <li id="part-4" onclick="select(4)">Marcha fúnebre para homilía</li>
+            <li id="part-5" onclick="select(5)">Hallelujah (la de Alexandra Burke)</li>
         </ul>
     </div>
 </section>
--- manager/scripts/player.js
+++ manager/scripts/player.js
@@ -3,17 +3,95 @@
  */
 
 function play() {
-    btPlay = document.getElementById('bt-play');
-    btPause = document.getElementById('bt-pause');
+    var btPlay = document.getElementById('bt-play');
+    var btPause = document.getElementById('bt-pause');
+    var selected = null;
+
+    for (var i = 0; i < playlist.children.length; i++) {
+        if (playlist.children[i].className == 'selected') {
+            playlist.children[i].className = '';
+            selected = playlist.children[(i + 1) % playlist.children.length];
+            break;
+        }
+    }
 
     btPlay.style.display = 'none';
     btPause.style.display = 'inline';
 }
 
 function pause() {
-    btPlay = document.getElementById('bt-play');
-    btPause = document.getElementById('bt-pause');
+    var btPlay = document.getElementById('bt-play');
+    var btPause = document.getElementById('bt-pause');
 
     btPlay.style.display = 'inline';
     btPause.style.display = 'none';
+}
+
+function stop() {
+    var btPlay = document.getElementById('bt-play');
+    var btPause = document.getElementById('bt-pause');
+    btPlay.style.display = 'inline';
+    btPause.style.display = 'none';
+}
+
+function previuos() {
+    var playlist = document.getElementById('current-playlist');
+    var selected = null;
+    var title = document.getElementById('part-name');
+    var btPlay = document.getElementById('bt-play');
+    var btPause = document.getElementById('bt-pause');
+
+    for (var i = 0; i < playlist.children.length; i++) {
+        if (playlist.children[i].className == 'selected') {
+            playlist.children[i].className = '';
+            selected = playlist.children[(i - 1 + playlist.children.length) % playlist.children.length];
+            break;
+        }
+    }
+
+    if (selected === null)
+        selected = playlist.firstElementChild;
+
+    selected.className = 'selected';
+    title.innerHTML = selected.innerHTML;
+}
+
+function next() {
+    var playlist = document.getElementById('current-playlist');
+    var selected = null;
+    var title = document.getElementById('part-name');
+    var btPlay = document.getElementById('bt-play');
+    var btPause = document.getElementById('bt-pause');
+
+    for (var i = 0; i < playlist.children.length; i++) {
+        if (playlist.children[i].className == 'selected') {
+            playlist.children[i].className = '';
+            selected = playlist.children[(i + 1) % playlist.children.length];
+            break;
+        }
+    }
+
+    if (selected === null)
+        selected = playlist.firstElementChild;
+
+    selected.className = 'selected';
+    title.innerHTML = selected.innerHTML;
+    btPlay.style.display = 'none';
+    btPause.style.display = 'inline';
+}
+
+function select(idpart) {
+    var playlist = document.getElementById('current-playlist');
+    var selected = document.getElementById('part-' + idpart);
+    var title = document.getElementById('part-name');
+    var btPlay = document.getElementById('bt-play');
+    var btPause = document.getElementById('bt-pause');
+
+    for (var i = 0; i < playlist.children.length; i++)
+        playlist.children[i].className = '';
+
+    selected.className = 'selected';
+    title.innerHTML = selected.innerHTML;
+    btPlay.style.display = 'none';
+    btPause.style.display = 'inline';
 }
