bccf7d54e63413a243e674b4777b1a189fe81046
Víctor Manuel Fernández Castro <vmfdez90@gmail.com>
2015-08-18 16:46:02 +0200

    Añadimos biblioteca WiringPi al sistema.

--- driver/scripts/preinstall.sh
+++ driver/scripts/preinstall.sh
@@ -6,11 +6,23 @@
 USER='organ'
 PACKAGES='libmysqlclient-dev'
 
+BIN=$(dirname $0)/../bin
+OBJ=$(dirname $0)/../obj
+
+SCRIPT=$(dirname $0)/organ
+WIRINGPI=$(dirname $0)/wiringPi
+WIRINGPI_TAR=$(dirname $0)/wiringPi.tar.gz
+
 apt-get install $PACKAGES
 
-mkdir -p $(dirname $0)/../obj $(dirname $0)/../bin
-chown pi:pi $(dirname $0)/../obj $(dirname $0)/../bin
-chmod a+x $(dirname $0)/organ
+mkdir -p $BIN $OBJ
+chown pi:pi $BIN $OBJ
+chmod a+x $SCRIPT
 
 adduser --quiet --group --system --no-create-home $USER
 adduser $USER $USER
+
+tar -xf $WIRINGPI_TAR -C $(dirname $0)
+chown -R pi:pi $WIRINGPI
+cd $WIRINGPI
+./build
