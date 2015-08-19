79aaac5ccc0c893cb70101d381634fa80ad48397
Víctor Manuel Fernández Castro <vmfdez90@gmail.com>
2015-08-19 10:28:57 +0200

    Implantación del control del rotary y el LCD.

---
+++ driver/include/peripherals.h
@@ -0,0 +1,13 @@
+/*
+ * Support for board peripherals that will control the Engineering Mode
+ * Victor Manuel Fernandez Castro
+ * 18 August 2015
+ */
+
+#ifndef PERIPHERALS_H
+#define PERIPHERALS_H
+
+// Initialize peripherals and start threads
+int periph_init();
+
+#endif
