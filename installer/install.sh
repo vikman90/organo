da1e896b0ad7fae6050b05433d412bdc6aa0c412
Víctor Manuel Fernández Castro <vmfdez90@gmail.com>
2015-09-04 12:37:33 +0200

    Adaptamos instalador a Apache 2.2

--- installer/install.sh
+++ installer/install.sh
@@ -14,7 +14,7 @@ ORGAN_GROUP="organ"
 
 SITE_SOURCE="organo"
 SITE_TARGET="001-organo"
-SITE_NAME="organo.ddns.net"
+SITE_NAME="localhost"
 SITE_ROOT="/home/pi/manager"
 SITE_POOL="/home/pi/midis"
 
@@ -35,11 +35,12 @@ SITE_ROOT_SED=$(echo $SITE_ROOT | sed 's/\//\\\//g')
 SITE_POOL_SED=$(echo $SITE_POOL | sed 's/\//\\\//g')
 
 sed -i "s/ServerName.*/ServerName $SITE_NAME/g" $(dirname $0)/$SITE_SOURCE
-sed -i "s/Define root.*/Define root $SITE_ROOT_SED/g" $(dirname $0)/$SITE_SOURCE
+sed -i "s/DocumentRoot.*/DocumentRoot $SITE_ROOT_SED/g" $(dirname $0)/$SITE_SOURCE
+sed -i "/<Directory \/>/b; s/<Directory.*/<Directory $SITE_ROOT_SED>/g" $(dirname $0)/$SITE_SOURCE
 sed -i "s/Alias \/pool.*/Alias pool $SITE_POOL_SED/g" $(dirname $0)/$SITE_SOURCE
 
 cp $(dirname $0)/$SITE_SOURCE /etc/apache2/sites-available
-ln -s /etc/apache2/sites-available/$SITE_SOURCE /etc/apache2/sites-enabled/$SITE_TARGET
+ln -fs /etc/apache2/sites-available/$SITE_SOURCE /etc/apache2/sites-enabled/$SITE_TARGET
 service apache2 reload
 
 ################################################################################
@@ -58,3 +59,4 @@ fi
 mysql -u$DB_USER -p < $(dirname $0)/$DB_SOURCE
 mkdir -p $SITE_ROOT $SITE_POOL
 chown $HOST_USER:$HOST_USER $SITE_ROOT $SITE_POOL
+chmod a+w $SITE_POOL
